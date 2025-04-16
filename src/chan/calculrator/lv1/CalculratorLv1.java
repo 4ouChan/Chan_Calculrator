daaf25f8c6cc0e66fb713482a13a625a00622c6a
4ouChan <kumho3983398@naver.com>
2025-04-16 19:59:49 +0900

    feat: 문자열을 구분하여 결과를 출력하는 기능 추가 및 계산 내용을 출력하는 출력문 삭제

--- src/chan/calculrator/lv1/CalculratorLv1.java
+++ src/chan/calculrator/lv1/CalculratorLv1.java
@@ -11,17 +11,37 @@ public class CalculratorLv1 {
 
         // 첫 번째 정수를 입력받는 코드
         System.out.print("첫 번째 정수를 입력하세요: ");
-        int indexOne = scanner.nextInt();
+        long indexOne = scanner.nextLong();
 
         // 두 번째 정수를 입력받는 코드
         System.out.print("두 번째 정수를 입력하세요: ");
-        int indexTwo = scanner.nextInt();
+        long indexTwo = scanner.nextLong();
         scanner.nextLine();
+
 
         // 사칙연산 기호를 입력받는 코드
         System.out.print("사칙연산 기호를 입력하세요: ");
         String operationSign = scanner.nextLine();
-        System.out.println("계산 내용: " + indexOne + " " + operationSign + " " + indexTwo);
+
+
+
+
+        // 입력받은 문자열이 사칙연산의 기호가 맞는지 구분하는 조건문 사칙연산의 기호가 아닐시 메시지를 출력해 예외처리
+        if (operationSign.equals("+")) {
+            long indexSum = indexOne + indexTwo;
+            System.out.println("계산 결과: " + indexOne + " " +  operationSign + " " + indexTwo + " = " + indexSum);
+        } else if (operationSign.equals("-")) {
+            long indexSub = indexOne - indexTwo;
+            System.out.println("계산 결과: " + indexOne + " " +  operationSign + " " + indexTwo + " = " + indexSub);
+        } else if (operationSign.equals("*")) {
+            long indexMul = indexOne * indexTwo;
+            System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexMul);
+        } else if (operationSign.equals("/")) {
+            double indexDiv = (double)indexOne / (double)indexTwo;
+            System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexDiv);
+        } else {
+            System.out.println("사칙연산의 기호가 아닙니다.");
+        }
 
     }
 }
