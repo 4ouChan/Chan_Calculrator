1a15433ab02c8dc8ae83b2731901ee3416439685
4ouChan <kumho3983398@naver.com>
2025-04-16 17:37:50 +0900

    feat: 사칙연산 기호ë를 입력받는 기능 추가

--- src/chan/calculrator/lv1/CalculratorLv1.java
+++ src/chan/calculrator/lv1/CalculratorLv1.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class CalculratorLv1 {
 
     public static void main(String[] args) {
+
+        // 스캐너 기능을 활용할 수 있는 변수 생성.
         Scanner scanner = new Scanner(System.in);
+
+        // 첫 번째 정수를 입력받는 코드
         System.out.print("첫 번째 정수를 입력하세요: ");
         int indexOne = scanner.nextInt();
 
+        // 두 번째 정수를 입력받는 코드
         System.out.print("두 번째 정수를 입력하세요: ");
         int indexTwo = scanner.nextInt();
+        scanner.nextLine();
 
+        // 사칙연산 기호를 입력받는 코드
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operationSign = scanner.nextLine();
+        System.out.println("계산 내용: " + indexOne + " " + operationSign + " " + indexTwo);
 
     }
 }
