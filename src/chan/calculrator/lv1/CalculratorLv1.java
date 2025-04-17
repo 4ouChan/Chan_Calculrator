a39c9a15fde3eb0a7099357c1563bc4174ae9155
4ouChan <kumho3983398@naver.com>
2025-04-17 16:32:22 +0900

    refactor: 나눗셈 연산 결과를 소숫점까지 표현하는 기능 제거 및 구조 개선

--- src/chan/calculrator/lv1/CalculratorLv1.java
+++ src/chan/calculrator/lv1/CalculratorLv1.java
@@ -11,8 +11,8 @@ public class CalculratorLv1 {
 
         long indexOne; // 첫 번째 정수를 담을 변수
         long indexTwo; // 두 번째 정수를 담을 변수
-        String checkIndex; // 정수를 문자열로 변환하여 음수인지 확인할 수 있게 도와주는 변수
-        String operationSign; // 사칙연산 기호를 담을 변수
+        long calcResult; // 계산 결과를 담을 변수
+        char operator; // 사칙연산 기호를 담을 변수
         String exit; // 종료 관련 문자를 입력받을 변수
 
 
@@ -22,11 +22,8 @@ public class CalculratorLv1 {
             System.out.print("첫 번째 정수를 입력하세요: ");
             indexOne = scanner.nextLong();
 
-            // 정수를 문자열로 변환
-            checkIndex = Long.toString(indexOne);
-
-            // 입력값의 첫 글자가 '-'일 경우 메시지 출력 후 반복문 종료
-            if (checkIndex.charAt(0) == '-') {
+            // indexOne의 값이 0보다 작을경우 메시지 출력 후 반복문 종료
+            if (indexOne < 0) {
                 System.out.println("양의 정수만 입력할 수 있습니다.");
                 break;
             }
@@ -36,11 +33,8 @@ public class CalculratorLv1 {
             indexTwo = scanner.nextLong();
             scanner.nextLine();
 
-            // 정수를 문자열로 변환
-            checkIndex = Long.toString(indexTwo);
-
-            // 입력값의 첫 글자가 '-'일 경우 메시지 출력 후 반복문 종료
-            if (checkIndex.charAt(0) == '-') {
+            // indexOne의 값이 0보다 작을경우 메시지 출력 후 반복문 종료
+            if (indexTwo < 0) {
                 System.out.println("양의 정수만 입력할 수 있습니다.");
                 break;
             }
@@ -48,33 +42,33 @@ public class CalculratorLv1 {
 
             // 사칙연산 기호를 입력받는 코드
             System.out.print("사칙연산 기호를 입력하세요: ");
-            operationSign = scanner.nextLine();
+            operator = scanner.next().charAt(0);
 
 
             // 입력받은 문자열이 사칙연산의 기호가 맞는지 구분하는 조건문 사칙연산의 기호가 아닐시 메시지를 출력해 예외처리
-            if (operationSign.equals("+")) {
-                long indexSum = indexOne + indexTwo;
-                System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexSum);
-            } else if (operationSign.equals("-")) {
-                long indexSub = indexOne - indexTwo;
-                System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexSub);
-            } else if (operationSign.equals("*")) {
-                long indexMul = indexOne * indexTwo;
-                System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexMul);
-            } else if (operationSign.equals("/")) {
+            if (operator == '+') {
+                calcResult = indexOne + indexTwo;
+            } else if (operator == '-') {
+                calcResult = indexOne - indexTwo;
+            } else if (operator == '*') {
+                calcResult = indexOne * indexTwo;
+            } else if (operator == '/') {
                 if (indexTwo == 0) {
                     System.out.println("나눗셈 연산 시, 두 번째 정수에 0을 입력할 수 없습니다.");
+                    break;
                 } else {
-                    double indexDiv = (double) indexOne / (double) indexTwo;
-                    System.out.println("계산 결과: " + indexOne + " " + operationSign + " " + indexTwo + " = " + indexDiv);
+                    calcResult = indexOne / indexTwo;
                 }
             } else {
                 System.out.println("사칙연산의 기호가 아닙니다.");
                 break;
             }
 
+            System.out.println("계산 결과: " + indexOne + " " + operator + " " + indexTwo + " = " + calcResult);
+
             // 입력받은 문자가 exit라면 프로그램을 종료하고, exit가 아니라면 계속 실행하는 코드
-            System.out.println("계속 하시겠습니까? (exit를 입력하여 종료.)");
+            System.out.print("계속 하시겠습니까? (exit를 입력하여 종료.): ");
+            scanner.nextLine();
             exit = scanner.nextLine();
             if (exit.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
